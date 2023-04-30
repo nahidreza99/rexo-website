b899e12ad68e569ba07ae973fb8870da198d0150
Nahid Reza <nahidreza99@gmail.com>
2023-04-30 17:56:03 +0600

    Added footer

--- src/pages/components/layout.js
+++ src/pages/components/layout.js
@@ -1,6 +1,8 @@
 import Head from "next/head"
 import Image from "next/image"
 import Link from "next/link"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faInstagram, faTwitter, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 export default function Layout({ children }){
   return (
@@ -13,7 +15,7 @@ export default function Layout({ children }){
           start conversations, and nurture loyalty without relying on social media or no-reply emails. Learn more about our packages, prices and privacy."/>
         <title>Rexo</title>
       </Head>
-      <header className="">
+      <header>
         <nav className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'>
           <div className="logo">
             <Link href='/'>
@@ -33,6 +35,26 @@ export default function Layout({ children }){
       <div className="container mx-auto max-w-7xl p-6 lg:px-8">
         { children }
       </div>
+      <footer className="mx-auto flex max-w-7xl items-center justify-between px-6 py-8 lg:px-8 lg:py-10 gs tv arf arn ckw cmp cmv dcv">
+        <div className="lh cjd ckk"><p className="text-gray-600 avg avx awj axm">© 2023 Rexo, Inc. All rights reserved.</p></div>
+        <div className="flex flex-row space-x-4">
+          <Link href='#' className="axk bkr group">
+            <FontAwesomeIcon icon={faFacebook} className="text-gray-500 nx rz w-6 h-6 p-0 group-hover:opacity-75"/>
+          </Link>
+          <Link href='#' className="axk bkr group">
+            <FontAwesomeIcon icon={faInstagram} className="text-gray-500 nx rz w-6 h-6 p-0 group-hover:opacity-75"/>
+          </Link>
+          <Link href='#' className="axk bkr group">
+            <FontAwesomeIcon icon={faTwitter} className="text-gray-500 nx rz w-6 h-6 p-0 group-hover:opacity-75"/>
+          </Link>
+          <Link href='#' className="axk bkr group">
+            <FontAwesomeIcon icon={faGithub} className="text-gray-500 nx rz w-6 h-6 p-0 group-hover:opacity-75"/>
+          </Link>
+          <Link href='#' className="axk bkr group">
+            <FontAwesomeIcon icon={faYoutube} className="text-gray-500 nx rz w-6 h-6 p-0 group-hover:opacity-75"/>
+          </Link>
+        </div>
+      </footer>
     </div>
   )
 }
